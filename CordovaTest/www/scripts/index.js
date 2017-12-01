3b4d3d30518ae5fe06007c9a77dbfe3aaea4b9f9
abrjak <jakob.abraham@gibmit.ch>
2017-12-01 15:38:22 +0100

    Added Navs and Menues

--- CordovaTest/www/scripts/index.js
+++ CordovaTest/www/scripts/index.js
@@ -1,24 +1,16 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
+﻿$(document).ready(function () {
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    $('.btn-login').click(function () {
+        verifyLogin();
+    });
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
+});
 
-    };
+function verifyLogin() {
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+    var username = $('.input-username').val();
+    var password = $('.input-password').val();
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    console.log(username);
+    console.log(password);
+}
